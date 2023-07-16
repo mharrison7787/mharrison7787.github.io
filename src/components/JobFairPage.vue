0f455db001785402e2967b0e3adb35ec1b2fbfc8
mharrison7787 <mitchellharrison42@gmail.com>
2023-07-16 12:06:25 -0400

    added picture grid for the job fair pictures, added details section and intersted companies section,and added padding on the job fair page

--- src/components/JobFairPage.vue
+++ src/components/JobFairPage.vue
@@ -18,6 +18,28 @@
             With a wide variety of job opportunities available for those that attend, the Job Fair is sure to provide
             an engaging and enriching experience to all attendees, making it an ideal platform for those looking to grow
             and develop their career in tech.
+        </p>
+
+        <h3 style="text-align: center;">Details</h3>
+        <p style="text-align: center;">The current set date for the fall 2023 job fair is October 18th or 19th. The
+            companies
+            attedning will be announced at a later date. Where in the smc multi-purpose room
+        </p>
+
+        <div class="container my-4">
+            <div class="row">
+                <div class="col-md-4" v-for="(image, index) in images" :key="index">
+                    <img :src="image.src" class="img-fluid mb-3" alt="Image">
+                </div>
+            </div>
+        </div>
+        <h3 style="text-align: center;">Companies Interested?</h3>
+        <p style="text-align: center;">
+            We we welcome any technology companies who are looking for potential employees and interns! Last year we had
+            over 5 companies come out and we
+            will always want more! Please reach out to us on our Contact page, and a TCN Representative will be sure to get
+            back to you.
+
 
         </p>
     </div>
@@ -29,8 +51,33 @@ export default {
     name: 'JobFairPage',
     components: {
         TCNnavbar,
+    },
+    data() {
+        return {
+            images: [
+                { src: require('../assets/jobfair-1-min-p-500.jpg') },
+                { src: require('../assets/jobfair-2-min-p-500.jpg') },
+                { src: require('../assets/jobfair-3-min-p-500.jpg') },
+                { src: require('../assets/jobfair-4-min-p-500.jpg') },
+                { src: require('../assets/jobfair-5-min-p-500.jpg') },
+                { src: require('../assets/jobfair-6-min.jpg') },
+                { src: require('../assets/jobfair-7-min.jpg') },
+                { src: require('../assets/jobfair-8-min.jpg') },
+                { src: require('../assets/jobfair-9-min.jpg') },
+            ]
+        };
     }
-}
+};
+
 </script>
 
-<style scoped></style>
+<style>
+.container {
+    margin-top: 20px;
+}
+
+/* Add custom CSS to space out the images and add a new line between rows */
+.row {
+    margin-bottom: 20px;
+}
+</style>

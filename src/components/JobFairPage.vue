17cddc95a1c11da9232b5fdd4bebecd7ae5ffba4
mharrison7787 <mitchellharrison42@gmail.com>
2023-07-16 10:51:17 -0400

    added all the pictures from the job fair, started to work on the job fair page

--- src/components/JobFairPage.vue
+++ src/components/JobFairPage.vue
@@ -5,42 +5,20 @@
             <br>
             <span style="font-size: 70%">
                 <br> TECHNOLOGY CAREER NETWORK</span> <br>
-            <span style="font-size: 200%">JOB FAIR </span>
+            <span style="font-size: 200%">JOB FAIR FALL 2023</span>
             <br>
         </h1>
 
-        <p style="text-align: center;">Welcome to the offical TCN Millersville Univeristy Website. Here at TCN we are a club
-            for professional development,
-            networking, and sharing resources with other technology majors.
+        <p style="text-align: center;">
+            TCN's Computer Science Job Fair, is back this year in the fall semester!
+            This is where club members can be exposed to some of the best local
+            companies in the tech industry in Lancaster to see what each one is all about!
             <br>
-            Our goal here is simple: To help everyone in the CS department! Whether your a new incoming freshman or a senior
-            ready to
-            graduate it can be difficult to know where to start in the industry and preparing for such.
-            Interviews, resumes, job market, interships, cover letters, linkedInm it all can be a lot to handle.
-            Thats where we come in! We want to help guide that processes so everyone can be more prepared for the real
-            world!
-            <br>
-            <br>
-            <br>
-            We host events geared to support career development for everyone in the CS & STEM majors at Millersville so all
-            are
-            welcome.
-            We will be having guest speakers, networking, and workshops such as resume-building and mock interviews
-            throughout
-            the fall 2023
-            and spring 2023 semester. The TCN is here for your opportunity to jumpstart your Technology Career!
-            <br>
-            <br>
-            <br>
-            Interested in joining the TCN?
-            You can officially join the club
-            by visiting Millersville's
-            <a href="https://getinvolved.millersville.edu/organization/tcn" style="color: #ecb431">Get Involved
-                Organizations</a>
-            As well, we have an actively growing <a href="https://discord.com/invite/sSRvdraWx9"
-                style="color: #473dd4">Discord
-                Server</a> where all
-            events and info is posted!
+            Attendees will be able to network with industry professionals and discover exciting career opportunities.
+            With a wide variety of job opportunities available for those that attend, the Job Fair is sure to provide
+            an engaging and enriching experience to all attendees, making it an ideal platform for those looking to grow
+            and develop their career in tech.
+
         </p>
     </div>
 </template>

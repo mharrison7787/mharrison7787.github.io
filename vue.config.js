86302b3a24a764ca07ceec4688cc69f190ce4b67
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-07-22 16:37:56 -0400

    add

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/mharrison7787.github.io/" : "/",
+    process.env.NODE_ENV === "production"
+      ? "https://mharrison7787.github.io/"
+      : "/",
 };

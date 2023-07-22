f91c8777fdc8dd7ea0249eafa77904f7f58af01f
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-07-22 16:08:15 -0400

    setting the base

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  base: "/mharrison7787.github.io/",
+  transpileDependencies: true,
+});

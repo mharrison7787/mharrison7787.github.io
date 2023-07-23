c4cab9553d7582a43bd6d8d9fa5d6374c480ed13
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-07-23 17:43:32 -0400

    "Changed the vue congig"

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/mharrison7787/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
 
 
